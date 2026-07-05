a82cb59f2e591088599f4c1d3cd70858e85db3b5
RAM VIJAY YADAV <vijay90054442298@gmail.com>
2026-07-05 19:23:17 +0530

    Update frontend web UI and deps

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ModernApp from './ModernApp';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ModernApp />
+  <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID}>
+    <ModernApp />
+  </GoogleOAuthProvider>
 );
 
